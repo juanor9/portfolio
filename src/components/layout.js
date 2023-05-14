97dd696b9f41339e0b59b7cc23c45a3d36508856
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-05-14 17:20:10 -0500

    Set basic structure for home page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,41 @@
-import * as React from 'react'
+import * as React from "react";
+import { header, header__navbar, main } from "./layout.module.scss";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <div>
-      <header>Juan Camilo Orjuela</header>
-      <main>
-        <h1>Main</h1>
-        {children}
-      </main>
+      <header className={header}>
+        <div>
+          <h1>Juan Camilo Orjuela</h1>
+          <p>Web developer</p>
+        </div>
+        <nav>
+          <ul className={header__navbar}>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Blog</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+      <main className={main}>{children}</main>
+      <footer>
+        <section>
+          <p>Juan Camilo Orjuela</p>
+          <p>© 2023, Built and designed by Juan Camilo Orjuela</p>
+        </section>
+        <section>
+          <p>Links</p>
+          <ul>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Blog</li>
+            <li>Contact</li>
+          </ul>
+        </section>
+      </footer>
     </div>
-  )
-}
+  );
+};
 
 export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,62 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import {
+  home__section,
+  home__hero,
+} from './index.module.scss'
 
 const IndexPage = () => {
   return (
     <Layout>
-      <p>Test paragraph</p>
+      <section className={`${home__section} ${home__hero}`}>
+        <p>Hi, I'm Juan Camilo Orjuela, web developer</p>
+        <div>
+          <ul>
+            <li>
+              Github
+            </li>
+            <li>
+              Linkedin
+            </li>
+            <li>
+              Behance
+            </li>
+            <li>
+              CV
+            </li>
+            <li>
+              Contact
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section className={home__section}>
+        <h2>My Skills</h2>
+        <ul>
+          <li>Javascript</li>
+          <li>Typescript</li>
+          <li>Python</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>SASS</li>
+          <li>React.js</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>Gatsby</li>
+          <li>Django</li>
+          <li>MySQL</li>
+          <li>MongoDB</li>
+          <li>GraphQL</li>
+          <li>SQLite</li>
+          <li>Wordpress</li>
+        </ul>
+      </section>
+      <section className={home__section}>
+        <h2>Projects</h2>
+      </section>
+      <section className={home__section}>
+        <h2>Contact</h2>
+      </section>
     </Layout>
   )
 }
