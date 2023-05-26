eaabb507161ef4b9fa9d13c25bfcadc095ceada8
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-05-26 18:07:00 -0500

    set kwes init inside a useEfect

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,8 +2,11 @@ import * as React from "react";
 import kwesforms from "kwesforms";
 
 const ContactForm = () => {
-  console.log(kwesforms);
-  kwesforms.init();
+  React.useEffect(() => {
+    if (typeof window !== "undefined") {
+      kwesforms.init();
+    }
+  }, []);
   return (
     <form style={{width:"100%"}} className="kwes-form" action="https://kwesforms.com/api/foreign/forms/1S09YWorOaq75jIP2ITZ">
       <label htmlFor="email">
