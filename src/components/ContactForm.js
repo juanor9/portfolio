379ccdd97b1eddf0f9e6e89da1df7bb859d1c385
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-05-26 18:00:52 -0500

    init kwes

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,7 +2,8 @@ import * as React from "react";
 import kwesforms from "kwesforms";
 
 const ContactForm = () => {
-  // kwesforms.init();
+  console.log(kwesforms);
+  kwesforms.init();
   return (
     <form style={{width:"100%"}} className="kwes-form" action="https://kwesforms.com/api/foreign/forms/1S09YWorOaq75jIP2ITZ">
       <label htmlFor="email">
