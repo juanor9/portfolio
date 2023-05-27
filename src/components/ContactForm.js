38b0a570ecab8d5c58d326e43327a690184079da
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-05-26 20:03:07 -0500

    polish useEffect

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,25 +1,31 @@
 import * as React from "react";
 import kwesforms from "kwesforms";
+import { useEffect } from "react";
 
 const ContactForm = () => {
-  React.useEffect(() => {
+  useEffect(() => {
     if (typeof window !== "undefined") {
       kwesforms.init();
     }
   }, []);
+
   return (
-    <form style={{width:"100%"}} className="kwes-form" action="https://kwesforms.com/api/foreign/forms/1S09YWorOaq75jIP2ITZ">
+    <form
+      style={{ width: "100%" }}
+      className="kwes-form"
+      action="https://kwesforms.com/api/foreign/forms/1S09YWorOaq75jIP2ITZ"
+    >
       <label htmlFor="email">
         Email
-        <input name="email" id="email" type="email"/>
+        <input name="email" id="email" type="email" />
       </label>
       <label htmlFor="message">
         Message
-        <textarea name="message" id="message" rows="15"/>
+        <textarea name="message" id="message" rows="15" />
       </label>
       <button type="submit">Submit</button>
     </form>
-  )
+  );
 };
 
-export default ContactForm
+export default ContactForm;
