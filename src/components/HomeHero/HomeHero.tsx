27ca74931a264d1c3ea674188b4099d9ff1551e5
Juan Camilo Orjuela <15643199+juanor9@users.noreply.github.com>
2025-07-24 15:12:17 -0500

    adjust layout for desktop

--- src/components/HomeHero/HomeHero.tsx
+++ src/components/HomeHero/HomeHero.tsx
@@ -5,28 +5,38 @@ import "./HomeHero.scss"
 
 const HomeHero = () => {
   return (
-    <section className="home-hero">
-      <div className="home-hero__image">
-        <picture className="home-hero__picture">
-          <img src="" alt="" />
-        </picture>
-      </div>
-      <div className="home-hero__content">
-        <h1 className="home-hero__title">Juan Camilo Orjuela</h1>
-        <h2 className="home-hero__subtitle">Front-end Developer</h2>
-        <StackButtons/>
-        <p className="home-hero__description">Desarrollador Front-End enfocado en accesibilidad, diseño limpio y código sostenible.</p>
-        <ul className="home-hero__links">
-          <li className="home-hero__link">
-            <PrimaryButton text="Ver proyectos" to="/projects" _isLink={true} _isDisabled={true} />
+    <section className="home-hero" id="home-hero">
+      <div className="home-hero__container">
+        <div className="home-hero__image">
+          <picture className="home-hero__picture">
+            <img src="/hero-banner-01.svg" alt="" />
+          </picture>
+        </div>
+        <div className="home-hero__content">
+          <h1 className="home-hero__title">Juan Camilo Orjuela</h1>
+          <h2 className="home-hero__subtitle">Front-end Developer</h2>
+          <StackButtons />
+          <p className="home-hero__description">Desarrollador Front-End enfocado en accesibilidad, diseño limpio y código sostenible.</p>
+          <ul className="home-hero__links">
+            <li className="home-hero__link">
+              <PrimaryButton text="Ver proyectos" to="/projects" _isLink={true} />
             </li>
-          <li className="home-hero__link">
-            <PrimaryButton text="Contactarme" to="/contact" _isLink={true} _isDisabled={true}/>
+            <li className="home-hero__link">
+              <PrimaryButton text="Contactarme" to="/contact" _isLink={true} />
             </li>
-          <li className="home-hero__link--fullwidth">
-            <PrimaryButton text="Descargar mi CV" to="/cv" _isLink={true} _isDisabled={true}/>
+            <li className="home-hero__link--fullwidth">
+              <a
+                href="/juan-camilo-orjuela-frontend-dev_cv.pdf"
+                className="primary-button"
+                target="_blank"
+                rel="noopener noreferrer"
+                download
+              >
+                Descargar mi CV
+              </a>
             </li>
-        </ul>
+          </ul>
+        </div>
       </div>
     </section>
   )
