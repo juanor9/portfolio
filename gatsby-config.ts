7eb0cdb2fcc8d29814ade858f13a659194605bd0
Juan Camilo Orjuela <orjuela9@gmail.com>
2025-07-29 11:43:03 -0500

    Revert "Feature/projects"

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,63 +1,34 @@
-import type { GatsbyConfig } from 'gatsby';
-
-// PASO 1: AÑADE ESTAS LÍNEAS AL INICIO
-import dotenv from 'dotenv';
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
-// ----------------------------------------
-
-import https from 'https';
+import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `juan-dev-portfolio`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Juan DEV Portfolio`,
+    siteUrl: `https://www.yourdomain.tld`
   },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [
-    'gatsby-plugin-image',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-mdx',
-    'gatsby-plugin-sitemap',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'juan-dev-portfolio',
-        short_name: 'Portfolio',
-        start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        display: 'standalone',
-        icon: 'src/images/icon.png',
-      },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
     },
-    'gatsby-plugin-svgr',
-    'gatsby-plugin-sass',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: './src/images/',
-      },
-      __key: 'images',
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
     },
-    {
-      resolve: 'gatsby-source-strapi',
-      options: {
-        // Ahora sí, esta variable tendrá valor
-        apiURL: process.env.STRAPI_API_URL,
-        collectionTypes: ['project'],
-        singleTypes: [],
-        accessToken: process.env.STRAPI_TOKEN,
-        // Y mantenemos la solución para el problema de SSL
-        axiosConfig: {
-          httpsAgent: new https.Agent({
-            rejectUnauthorized: false,
-          }),
-        },
-      },
-    },
-  ],
+    __key: "pages"
+  }]
 };
 
 export default config;
